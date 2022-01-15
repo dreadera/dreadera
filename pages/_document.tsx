9d821d1255ddcb8decdd0085d7b95c389ebad120
umi wimalawardana <dreadera@outlook.com>
2022-01-15 00:34:01 -0800

    feat(deps): added prettier and formatted existing files.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,29 +1,26 @@
-import React from "react";
-import Document, { Html, Head, Main, NextScript } from "next/document";
+import React from 'react'
+import Document, { Html, Head, Main, NextScript } from 'next/document'
 
 class DocumentTemplate extends Document {
-  render() {
-    return (
-      <Html>
-        <Head>
-          <meta
-            name="viewport"
-            content="initial-scale=1.0, width=device-width"
-          />
-        </Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-
-        <link
-          href="https://fonts.googleapis.com/css2?family=Raleway:wght@100;300;400;700&display=swap"
-          rel="stylesheet"
-        />
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+    render() {
+        return (
+            <Html>
+                <Head>
+                    <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+                </Head>
+                <link rel="preconnect" href="https://fonts.gstatic.com" />
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link
+                    href="https://fonts.googleapis.com/css2?family=Raleway:wght@100;300;400;700&display=swap"
+                    rel="stylesheet"
+                />
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        )
+    }
 }
-export default DocumentTemplate;
+
+export default DocumentTemplate
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,16 @@
-import type { NextPage } from "next";
-import Head from "next/head";
+import type { NextPage } from 'next'
+import Head from 'next/head'
 
-const Home: NextPage = () => {
-  return (
+const Home: NextPage = () => (
     <div>
-      <Head>
-        <title>{`//dreadera`}</title>
-        <meta name="description" content="dreadera" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+        <Head>
+            <title>{`//dreadera`}</title>
+            <meta name="description" content="dreadera" />
+            <link rel="icon" href="/favicon.ico" />
+        </Head>
 
-      <main />
+        <main />
     </div>
-  );
-};
+)
 
-export default Home;
+export default Home
